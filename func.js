3ea6e7e56c881ba7b3d0ee800f542035c0b8163d
Ben Burford (MP) <jbburf@gmail.com>
2018-05-11 12:46:10 -0500

    Set inputs by passed in URI variables
    Make, model and version of LED can be pre-populated by passing the correct value to the page via URI variables. The function now checks these and sets the dropdowns accordingly and loads the LED info section if all of the inputs are specified and pass validation.

--- func.js
+++ func.js
@@ -2,6 +2,52 @@
 // UI functions
 //
 
+function pageLoad(){
+  var maker = decodeURI(getURIParams("mk"));
+  var model = decodeURI(getURIParams("md"));
+  var version = decodeURI(getURIParams("vr"));
+  var makerPassed = (maker !== undefined && maker !== null);
+  var modelPassed = (model !== undefined && model !== null);
+  var versionPassed = (version !== undefined && version !== null);
+
+  if(makerPassed){
+    loadDropDown("inputGroupSelect01",getLEDMakers());
+    try{ document.getElementById("inputGroupSelect01").options[getOptionIndex("inputGroupSelect01",maker)].selected = true; }
+    catch(err){
+      console.log("Failed to set LED maker dropdown: " + err.message);
+      makerPassed = false; }
+    loadDropDown("inputGroupSelect02",getLEDModels(maker));
+  }
+
+  if(modelPassed){
+    loadDropDown("inputGroupSelect02",getLEDModels(maker));
+    try{ document.getElementById("inputGroupSelect02").options[getOptionIndex("inputGroupSelect02",model)].selected = true; }
+    catch(err){
+      console.log("Failed to set LED model dropdown: " + err.message);
+      modelPassed = false; }
+    loadDropDown("inputGroupSelect03",getLEDVersions(model));
+  }
+
+  if(versionPassed){
+    loadDropDown("inputGroupSelect03",getLEDVersions(model));
+    try{ document.getElementById("inputGroupSelect03").options[getOptionIndex("inputGroupSelect03",version)].selected = true; }
+    catch(err){
+      console.log("Failed to set LED version dropdown: " + err.message);
+      versionPassed = false; }
+  }
+
+  if(makerPassed && modelPassed && versionPassed) { showLEDInfo(); }
+}
+
+function getOptionIndex(elementID,optionValue){
+  for(var x in document.getElementById(elementID).options){
+    if(document.getElementById(elementID).options[x].value == optionValue){
+      return x;
+    }
+  }
+  console.log("Element: \"" + elementID + "\" does not contain an option with value= \"" + optionValue + "\".");
+}
+
 function loadDropDown(dropDownId, contents){
   var targetDropDown = document.getElementById(dropDownId);
 
@@ -55,8 +101,8 @@ function showLEDInfo(){
 
   LEDobj = new LED("LED1",model,version);
 
-  var power = roundTo(LEDobj.nomVf * LEDobj.nomCurrent/1000,1);
-  var efficacy = roundTo(LEDobj.nomFlux / power,0);
+  var power = roundTo(LEDobj.nom.vf * LEDobj.nom.i/1000,1);
+  var efficacy = roundTo(LEDobj.nom.flux / power,0);
 
   var tableVis = document.getElementById("LED-info");
   if(tableVis.style.display = "none"){ tableVis.style.display = "block";}
@@ -64,16 +110,16 @@ function showLEDInfo(){
   document.getElementById("LEDImage").src = "assets/LED-" + model + ".jpg";
   document.getElementById("partNumber").innerHTML = LEDobj.partNumber;
   document.getElementById("DSLink").href = LEDobj.DSLink;
-  document.getElementById("nomPower").innerHTML = LEDobj.nomVf + " volts, " + LEDobj.nomCurrent + "mA (" + power + " watts)";
-  document.getElementById("nomFlux").innerHTML = LEDobj.nomFlux + " lm";
-  document.getElementById("minFlux").innerHTML = LEDobj.minFlux + " lm";
-  document.getElementById("TjTc").innerHTML = LEDobj.TjTc;
-  document.getElementById("temp").innerHTML = LEDobj.temp + "&#8451;";
+  document.getElementById("nomPower").innerHTML = LEDobj.nom.vf + " volts, " + LEDobj.nom.i + "mA (" + power + " watts)";
+  document.getElementById("nomFlux").innerHTML = LEDobj.nom.flux + " lm";
+  document.getElementById("minFlux").innerHTML = LEDobj.min.flux + " lm";
+  document.getElementById("TjTc").innerHTML = LEDobj.nom.TjTc;
+  document.getElementById("temp").innerHTML = LEDobj.nom.temp[LEDobj.nom.TjTc] + "&#8451;";
   document.getElementById("nomEfficacy").innerHTML = efficacy + " lm/W";
 
-  document.getElementById("tempSource").innerHTML = LEDobj.TjTc + ": ";
-  setSlider("tempSlider",-40,25,125);
-  setSlider("currentSlider",LEDobj.minCurrent,LEDobj.nomCurrent,LEDobj.maxCurrent);
+  document.getElementById("tempSource").innerHTML = LEDobj.nom.temp[LEDobj.nom.TjTc] + ": ";
+  setSlider("tempSlider",-40,25,125);   // NEED to update this to set slider and toggle based on LED inputs
+  setSlider("currentSlider",LEDobj.min.i,LEDobj.nom.i,LEDobj.max.i);
 }
 
 function calcResults(){
@@ -86,7 +132,7 @@ function calcResults(){
   document.getElementById("calcFlux").innerHTML = roundTo(flux,-1);
   document.getElementById("calcPower").innerHTML = roundTo(power,1);
   document.getElementById("calcEfficacy").innerHTML = roundTo(flux/power,1);
-  document.getElementById("calcLifeTime").innerHTML = "some hours...";
+  document.getElementById("calcLifeTime").innerHTML = LEDobj.estLifeTime();
 
   console.log("Flux(I): " + roundTo(currentToFlux(LEDobj.currentNow),3) + ", Flux(Tj): " + roundTo(TjToFlux(LEDobj.tempNow),3) + ", Vf: " + vf + ", I: " + LEDobj.currentNow);
 }
@@ -107,11 +153,10 @@ class LED {
       this.make = "";
       this.model = "";
       this.version = "";
-      this.nomVf = 0
-      this.nomI = 0;
-      this.nomTemp = 25;
-      this.notLum = 0;
-      this.nomPow = 0;
+      this.nom.vf = 0
+      this.nom.i = 0;
+      this.nom.temp["Tj"] = 25;
+      this.nom.flux = 0;
       this.flux_of_I = 0;
       this.flux_of_Tj_C = 0;
       this.vf_of_I_C = 0;
@@ -122,17 +167,18 @@ class LED {
       temp = getVersionAttrs(model,version);
       for(var x in temp){ this[x] = temp[x]; }
     }
-    this.current = this.nomI;
-    this.vf = this.nomVf;
-    this.temp = this.nomTemp;
+
+      this.now = this.nom;
+
+    // Set Tj based on Tc or Tc based on Tj if full specs are not given
   }
 
 // need recursive function to solve for stats when changing I or Tj
 
-  setCurrent(I){ this.current = I; }
-  changeCurrent(I){ this.current +=I; }
-  setTemp(temp){this.temp = temp;}
-  changeTemp(temp){this.temp +=temp;}
+  setCurrent(I){ this.now.i = I; }
+  changeCurrent(I){ this.now.i +=I; }
+  setTemp(temp){this.now.temp = temp;}
+  changeTemp(temp){this.now.temp +=temp;}
 
   getLum(current, temp){
   // return lumens based on current and temperature
@@ -170,7 +216,7 @@ class LED {
       vfOffSet += this.vf_of_Tj[x] * Math.pow(temp,x);
     }
 
-    this.temp = this.nomTemp + vfOffSet
+    this.now.temp[1] = this.nom.temp[1] + vfOffSet;
   }
 
   vf_of_I(current){
@@ -180,12 +226,41 @@ class LED {
     }
     return vfFactor;
   }
+
+  estLifeTime(I,temp){
+    if(I === undefined) { I = this.non.i; }
+    if(temp === undefined) { temp = this.nom.temp };
+
+    return "50,000";
+  }
+
 } // end of LED object definition
 
 //
 // General functions
 //
 
+function getURIParams(param) {
+  // from https://www.creativejuiz.fr/blog/en/javascript-en/read-url-get-parameters-with-javascript
+  var vars = {};
+	window.location.href.replace( location.hash, '' ).replace(
+		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+		function( m, key, value ) { // callback
+			vars[key] = value !== undefined ? value : '';
+		}
+	);
+
+	if ( param ) {
+		return vars[param] ? vars[param] : null;
+	}
+	return vars;
+}
+
 function roundTo(number,places){
   return Math.round(number * Math.pow(10,places)) / Math.pow(10,places);
+}
+
+function formatInt(int,places){
+  // fromat so that 50000 becomes 50,000
+  return roundTo(int,places);
 }
