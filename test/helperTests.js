4bb0939c47d5f633be66323263eb29bedb9523c6
Ben Burford (MP) <jbburf@gmail.com>
2018-05-22 22:37:18 -0500

    Started writing unit tests with Mocha

---
+++ test/helperTests.js
@@ -0,0 +1,7 @@
+var assert = chai.assert;
+
+describe("dropDownSelector", function(){
+  it("should return SOMETHING if ID exists");
+  it("should return a proper error if ID does not exist");
+  it("should return a proper error if ID is not a string");
+});
